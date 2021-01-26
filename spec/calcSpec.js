08afacf211ea8cbb760670cefb630c4679969f8b
roryogorman <roryogorman@hotmail.com>
2021-01-26 20:42:40 +0000

    Jasmine Testing Methods

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,15 +1,28 @@
 /* Describe a Calculator */
 describe("Calculator", function() {
     /* Describe the test we want to perform */
+    beforeEach(function() {     /*this command resets the value to zero at each function call - prevents aggregation */
+        calc = new Calculator;
+    });
     describe("Addition tests", function() {
         /* Make our assertions */
         it("should return 42", function() {
             /* What we expect */
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
         /* Adding in additional tests - put these in this function here, following the others */
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
+        });
+        /* Defensive Programming - adding test for receiving one number, strings, etc */
+        it("should return an error if we don't supply two numbers", function() {
+            spyOn(window,"alert");
+            calc.add("Hichhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
 });
